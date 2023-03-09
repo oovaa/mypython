e9825ddeffb09192c808e51b20155545ab8121a3
oovaa <omarvx211@gmail.com>
2023-03-09 19:16:40 +0200

    roman to int

--- exs.py
+++ exs.py
@@ -6,25 +6,28 @@ import random
 import re
 import sys
 
-n = int(input())
-if n%2 ==1 :
-    print('Weird')
-elif n %2 ==0 and n >= 2 and n <= 5:
-    print('Not Weird')
-elif n %2 ==0 and n >= 6 and n <= 20:
-    print('Weird')
-elif n %2 ==0 and n > 20 : 
-    print('Not Weird')
+
+
+
+# n = int(input())
+# if n%2 ==1 :
+#     print('Weird')
+# elif n %2 ==0 and n >= 2 and n <= 5:
+#     print('Not Weird')
+# elif n %2 ==0 and n >= 6 and n <= 20:
+#     print('Weird')
+# elif n %2 ==0 and n > 20 : 
+#     print('Not Weird')
     
-n = int(raw_input().strip())
-if n%2==1:
-    print ("Weird")    
-elif n%2 == 0 and 2 <= n <= 5:
-    print ("Not Weird")
-elif n%2 == 0 and 6 <= n <= 20:
-    print ("Weird")
-else:
-    print ("Not Weird")
+# n = int(raw_input().strip())
+# if n%2==1:
+#     print ("Weird")    
+# elif n%2 == 0 and 2 <= n <= 5:
+#     print ("Not Weird")
+# elif n%2 == 0 and 6 <= n <= 20:
+#     print ("Weird")
+# else:
+#     print ("Not Weird")
     
 
 # if __name__ == '__main__':
--- test.py
+++ test.py
@@ -1,8 +1,86 @@
-import random
-import string
+def romanToInt(s: str) -> int:
+    val = {'I'        :     1,
+'V'      :       5,
+'X'       :      10,
+'L'        :     50,
+'C'         :    100,
+'D'          :   500,
+'M'           :  1000}
+    ans = 0
+    i=0
+    j=1
+    while j < len(s):
+      if val[s[j]]>val[s[i]]:
+          ans +=val[s[j]]-val[s[i]] 
+          j+=2
+          i+=2
+      else:
+          ans +=val[s[i]]
+          i+=1
+          j+=1
+    
+    return ans
+          
+          
+        
+    
+    
+    
+print(romanToInt("III"))
+
+    
 
 
 
+
+
+
+# def isPalindrome(x: int) -> bool:
+#   x = str(x)
+#   l = len(x)-1
+#   s = 0
+#   while s < len(x)/2:
+#     if x[s] != x[l]:
+#       return False
+#     s+=1
+#     l-=1
+#   return True
+# print(isPalindrome(10))
+
+
+# def twoSum(self, nums: list[int], target: int) -> list[int]:
+#  i= 0
+#  hold = []
+#  while i < len(nums):
+#     j=0
+#     while j < len(nums):
+#        if j != i and nums[i]+ nums[j] == target:
+#          hold.append(i)
+#          hold.append(j)
+#          return hold 
+#        j+=1
+#     i+=1
+         
+# ls = [3,2,4]
+# t=6
+# print(twoSum(4,ls,t))
+
+
+
+
+# t=int(input('enter fb rank :'))
+# a=b=0
+# x=1
+# for i in range(t):
+#     a=b
+#     b=x
+#     x=a+b  
+#     print(x)
+
+
+
+# import random
+# import string
  
 # chars = string.punctuation+string.digits+string.ascii_letters+" "
 # ls = list(chars)
