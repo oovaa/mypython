3a4802f672dd9bb9f0edd06378b26ef89db86fed
oovaa <omarvx211@gmail.com>
2024-03-02 17:51:01 +0200

    Add code to extract project information from HTML response

--- alx-hunt/request.py
+++ alx-hunt/request.py
@@ -40,9 +40,26 @@ if login_response.ok:
 
     response = session.get('https://intranet.alxswe.com')
 
+    projects_page = session.get('https://intranet.alxswe.com/projects/current')
     # Get the content of the response
     content = response.content
 
     # Write the content to a file
-    with open('alx.html', 'wb') as f:
-        f.write(content)
+    # with open('alx.html', 'wb') as f:
+    #     f.write(content)
+
+    # Assume content is the HTML content you've fetched
+    soup = BeautifulSoup(content, 'html.parser')
+
+    # Find all anchor tags with hrefs that start with "/projects/"
+    project_tags = soup.find_all(
+        'a', href=lambda href: href and href.startswith('/projects/'))
+
+    # Extract the project IDs and names from the hrefs
+    project_info = {(tag['href'].split('/')[-1], tag.text)
+                    for tag in project_tags}
+
+    # print(project_info)
+
+    with open('alx.json', 'w') as file:
+        json.dump(dict(project_info), file, indent=3)
